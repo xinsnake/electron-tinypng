90182bbb46bcab85a29e08addd22e3ee2a2a4b2d
Xinyun Zhou <me@xyzhou.com>
2016-09-20 17:59:26 +1000

    more things done

---
+++ index.js
@@ -0,0 +1,9 @@
+let app = angular.module('electronTinypng', ['mui', 'ngFileUpload'])
+
+app.controller('mainController', ($scope) => {
+    console.log('mainController');
+})
+
+app.controller('optionController', ($scope) => {
+    console.log('optionController');
+})
--- main.js
+++ main.js
@@ -6,10 +6,13 @@ let win
 
 function createWindow () {
     // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600})
+    win = new BrowserWindow({width: 800, height: 600, resizable: true})
 
     // and load the index.html of the app.
     win.loadURL(`file://${__dirname}/index.html`)
+
+    // remove menu
+    // win.setMenu(null)
 
     // Open the DevTools.
     win.webContents.openDevTools()
